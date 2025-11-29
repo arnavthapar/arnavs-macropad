1c8d8efd3a0c96c2d64b7bffe15fe9637c31d676
Arnav Thapar <arnavthapar11@gmail.com>
2025-11-29 13:35:24 -0500

    firmware

---
+++ Firmware/main.py
@@ -0,0 +1,33 @@
+import board
+import digitalio
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+
+keyboard = KMKKeyboard()
+
+# Switch pins
+switch_pins = [board.D7, board.D11, board.D10, board.D9, board.D8]
+
+keyboard.matrix = KeysScanner(
+    pins=switch_pins,
+    value_when_pressed=False  # pull-ups
+)
+
+keyboard.keymap = [
+    [KC.A, KC.B, KC.C, KC.D, KC.E]
+]
+
+# LEDs
+led1 = digitalio.DigitalInOut(board.D5)
+led1.direction = digitalio.Direction.OUTPUT
+
+led2 = digitalio.DigitalInOut(board.D6)
+led2.direction = digitalio.Direction.OUTPUT
+
+# Turn on LEDs when keyboard starts
+led1.value = True
+led2.value = True
+
+if __name__ == "__main__":
+    keyboard.go()
